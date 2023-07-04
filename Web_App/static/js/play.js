85a082d44a2984fdebfccd04428ac66882b122a9
serioslydude <aniganta7@gmail.com>
2023-07-04 08:50:48 +0530

    Starting

--- Web_App/static/js/play.js
+++ Web_App/static/js/play.js
@@ -1,2 +1,6 @@
-// Will do the job oof map.ks and guessedpage.js in a single page
+// Will do the job of map.ks and guessedpage.js in a single page
 // all data most be stored here instead of webapp as webapp cant have any information vars
+
+var guessedCountry;
+var correctCountry;
+
